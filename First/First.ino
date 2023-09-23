79561514de4b0375e55974279e1767f1c6b81d1e
blevin04 <114503019+blevin04@users.noreply.github.com>
2023-09-23 14:19:42 +0300

    Update First.ino

--- First/First.ino
+++ First/First.ino
@@ -2,25 +2,32 @@ int dataPin=8;
 int latchPin=9;
 int clockPin=10;
 
-byte value=0;
+int value[16];
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(dataPin,OUTPUT);
 pinMode(latchPin,OUTPUT);
 pinMode(clockPin,OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i=0; i<8;i++){
-bitSet(value,i);
-updateShiftRegister();
+  for(int i=0; i<16;i++){
+    
   }
 
 }
-void updateShiftRegister(){
-  digitalWrite(latchPin,LOW);
-  shiftOut(dataPin,clockPin,LSBFIRST, value);
-  digitalWrite(latchPin,HIGH);
+
+
+void shift(int pin) {
+  digitalWrite(latchPin, LOW);
+
+  // shift the bits out:
+  shiftOut(dataPin, clockPin,MSBFIRST, pin>>8);
+  shiftOut(dataPin, clockPin,MSBFIRST, pin);
+  // turn on the output so the LEDs can light up:
+  digitalWrite(latchPin, HIGH);
+  delay(100);
 }
