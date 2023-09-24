812088b4125137c1f18a52a59970d9eea804079d
blevin04 <114503019+blevin04@users.noreply.github.com>
2023-09-24 16:32:29 +0300

    Update First.ino

--- First/First.ino
+++ First/First.ino
@@ -2,7 +2,7 @@ int dataPin=8;
 int latchPin=9;
 int clockPin=10;
 
-int value[16];
+short val=0b0000000000000000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,10 +14,14 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int i=0;i<16;i++){
+    select(val,i,HIGH);
+    shift(val);
+    delay(250);
+  }
 }
 void shift(int pin) {
   digitalWrite(latchPin, LOW);
-
   // shift the bits out:
   shiftOut(dataPin, clockPin,MSBFIRST, pin>>8);
   shiftOut(dataPin, clockPin,MSBFIRST, pin);
@@ -26,21 +30,24 @@ void shift(int pin) {
   delay(100);
 }
 
-void select(byte disp, int post, bool state){
-byte base = 0b0000000000000001;
-  switch (post) {
-    case 0: if(disp & base){
-      break;
+void select(short bit, int index, bool state){
+short baseHigh = 0b0000000000000001;
+short baseLow =0b1111111111111110;
+  if(bit & baseHigh<<index){
+     if(state){
+      return bit;
+     }else{
+      short tempBit = baseLow<<index;
+      short identity = tempBit | (~tempBit>>1);
+      bit &= identity;
+      return bit;
+     }
     }else{
-      disp = disp | base;
-      
+      if(state){
+        bit |=  baseHigh<<index;
+        return bit;
+      }else{
+        return bit;
+      }
     }
-    break;
-    case 1: if(disp & base<<1){
-      break;
-    }else{
-      disp = disp | base<<1;
-    }
-    case 2
-  }
 }
