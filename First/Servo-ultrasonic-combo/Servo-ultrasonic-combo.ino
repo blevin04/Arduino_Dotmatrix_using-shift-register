2c33f93153181c6b3359fdc0b219db90021f1d85
blevin04 <114503019+blevin04@users.noreply.github.com>
2023-10-04 15:14:39 +0300

    Update Servo-ultrasonic-combo.ino

--- First/Servo-ultrasonic-combo/Servo-ultrasonic-combo.ino
+++ First/Servo-ultrasonic-combo/Servo-ultrasonic-combo.ino
@@ -1,3 +1,12 @@
+#include <Servo.h>
+#include <NewPing.h>
+float distance,duration;
+int iterations =12;
+int trigPin=9;
+int echoPin=10;
+int maxDist=400;
+Servo myServo;
+NewPing Sonic1 (trigPin,echoPin,maxDist);
 void setup() {
   // put your setup code here, to run once:
 
