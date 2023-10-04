e7b6bd14145de9f6c449782070502ee946e389e6
blevin04 <114503019+blevin04@users.noreply.github.com>
2023-10-04 15:49:37 +0300

    Update Servo-ultrasonic-combo.ino

--- First/Servo-ultrasonic-combo/Servo-ultrasonic-combo.ino
+++ First/Servo-ultrasonic-combo/Servo-ultrasonic-combo.ino
@@ -5,14 +5,34 @@ int iterations =12;
 int trigPin=9;
 int echoPin=10;
 int maxDist=400;
+int servoPin=8;
+int distArray[180/5];
 Servo myServo;
 NewPing Sonic1 (trigPin,echoPin,maxDist);
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(servoPin);
+ Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ for(int i=0;i<=180;i+=5){
+  myServo.write(i);
+  duration=Sonic1.ping_median(iterations);
+  distance =(duration/2)*0.0341;
+  distArray[i]=distance;
+  Serial.print(distArray[i]);
+  Serial.println("cm");
+  delay (120);
+ }
+ for(int i=180;i>=0;i-=5){
+  myServo.write(i);
+  duration=Sonic1.ping_median(iterations);
+  distance =(duration/2)*0.0341;
+  distArray[i]=distance;
+  Serial.print(distArray[i]);
+  Serial.println("cm");
+  delay (120);
+ }
 }
